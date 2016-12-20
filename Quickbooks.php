7e2ec94fd9dd75c576249b76293e48cf2796661d
Caleb Krivoshein <caleb.krivoshein@vitalstorm.com>
2016-12-20 14:44:50 -0600

    update config require

--- Quickbooks.php
+++ Quickbooks.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('config.php');
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'config.php');
 
 /**
  * This class is required to setup prerequisites for Quickbooks SDK.
@@ -14,6 +14,19 @@ class Quickbooks
      */
     public static function begin()
     {
-
+    	spl_autoload_register(function ($name) {
+            $loadableClasses = [
+                'OAuthRequestValidator' => 'Core/ServiceContext.php',
+            ];
+            if (array_key_exists($name, $loadableClasses)) {
+                include_once(PATH_SDK_ROOT . $loadableClasses[$name]);
+            } else if (file_exists(PATH_SDK_ROOT . 'Core/' . $name . '.php')) {
+                include_once(PATH_SDK_ROOT . 'Core/' . $name . '.php');
+            } else if (file_exists(PATH_SDK_ROOT . 'Core/RestCalls/' . $name . '.php')) {
+                include_once(PATH_SDK_ROOT . 'Core/RestCalls/' . $name . '.php');
+            } else if (file_exists(PATH_SDK_ROOT . 'DataService/' . $name . '.php')) {
+                include_once(PATH_SDK_ROOT . 'DataService/' . $name . '.php');
+            }
+        });
     }
 }
